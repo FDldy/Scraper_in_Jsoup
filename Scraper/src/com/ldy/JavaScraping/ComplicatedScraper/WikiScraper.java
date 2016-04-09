95da8e57e05c60cd9ae831f0cc2409ec9b7dec5b
FDldy <FDldy@users.noreply.github.com>
2016-04-09 14:51:11 +0800

    update complicatedScraper on Apr 9

---
+++ Scraper/src/com/ldy/JavaScraping/ComplicatedScraper/WikiScraper.java
@@ -0,0 +1,65 @@
+package com.ldy.JavaScraping.ComplicatedScraper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class WikiScraper {
+	private static Random generator;
+	public static void main(String[] args) {
+		//Creates a new random number generator using a single seed
+		generator =	 new Random(31415926);
+		//call scrapeTopic method to catch uri "/wiki/java"
+		scrapeTopic("/wiki/Java");
+	}
+		//create scrapeTopic method to parse html object:
+	public static void scrapeTopic(String url) {
+		String html = getUrl("https://en.wikipedia.org"+url);
+		Document doc = Jsoup.parse(html);
+		Elements links = doc.select("#mw-content-text [href~=^/wiki/((?!:).)*$]");
+		if(links.size()==0){
+			System.out.println("No links found at "+ url +". Going back to Java!");
+			scrapeTopic("/wiki/Java");
+		}
+		int r = generator.nextInt(links.size());
+		System.out.println("Random link is:"
+				+links.get(r).text() + " at url:"
+				+links.get(r).attr("href"));
+		scrapeTopic(links.get(r).attr("href"));
+	}
+		//getUrl method:
+	public static String getUrl(String url) {
+		URL urlObj =null;
+		try {
+			urlObj = new URL(url);
+		} catch (MalformedURLException e) {
+			System.out.println("The url was malformed!");
+			return "";
+		}
+		URLConnection urlCon = null;
+		BufferedReader in = null;
+		String outputText="";
+		try {
+			urlCon = urlObj.openConnection();
+		in = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+		String line="";
+			while ((line=in.readLine())!=null){
+				outputText+=line;
+			}
+			in.close();
+		
+		} catch (IOException e) {
+			System.out.println("There was an error connecting to URL");
+			return "";
+		}
+		return outputText;
+	}
+}
